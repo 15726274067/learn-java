fd21b139406b3aa22017704bfb980681f4be15c1
zhutao <693117552@qq.com>
2019-02-20 10:07:15 +0800

    thread

---
+++ chapter1-java-core/src/main/java/com/zhutao/javacore/thread/ThreadMain.java
@@ -0,0 +1,61 @@
+package com.zhutao.javacore.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019/2/19 15:45
+ * @Version 1.0
+ */
+public class ThreadMain {
+    public static void main(String[] args){
+//        createThread();
+
+        createFutureTask();
+    }
+
+    public static void createThread(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("create new thread");
+            }
+        });
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("new thread with lambda");
+        });
+
+        thread.start();
+
+        thread1.start();
+    }
+
+    public static void createFutureTask() {
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return 1;
+            }
+        });
+
+        Thread thread = new Thread(futureTask);
+
+        thread.start();
+
+        // false
+        System.out.println("isDone: " +futureTask.isDone());
+        try {
+            // 1
+            System.out.println("get result: " + futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        // true
+        System.out.println("isDone: " +futureTask.isDone());
+    }
+}
---
+++ chapter1-java-core/src/main/java/com/zhutao/javacore/thread/ThreadPoolMain.java
@@ -0,0 +1,82 @@
+package com.zhutao.javacore.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019/2/19 15:59
+ * @Version 1.0
+ */
+public class ThreadPoolMain {
+
+    private static boolean done = false;
+
+    private static volatile boolean volatileDone = false;
+
+    public static void main(String[] args){
+//        createThreadPool();
+
+        testNonVolatile();
+    }
+
+
+    /**
+     * 主要有三种 Executor：
+     *
+     * CachedThreadPool：一个任务创建一个线程；
+     * FixedThreadPool：所有任务只能使用固定大小的线程；
+     * SingleThreadExecutor：相当于大小为 1 的 FixedThreadPool。
+     */
+    public static void createThreadPool(){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i=0; i<10; i++){
+            executorService.execute(() -> {
+                System.out.println("newCachedThreadPool ");
+            });
+        }
+
+        executorService.shutdown();
+    }
+
+    // 当所有非守护线程结束时，程序也就终止，同时会杀死所有守护线程
+    public static void daemon(){
+        Thread thread = new Thread(() -> {
+            System.out.println("test daemon");
+        });
+
+        // 设置为后台守护进程
+        thread.setDaemon(true);
+    }
+
+    public static void sleep(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testNonVolatile(){
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i< 1000; i++){
+                System.out.println(i);
+            }
+            done = true;
+        });
+
+        Thread thread1 = new Thread(() -> {
+            int i = 0;
+            while (!done) i++;
+            System.out.println("goodBye " + i);
+        });
+
+        ExecutorService executorService =Executors.newCachedThreadPool();
+
+        executorService.execute(thread);
+
+        executorService.execute(thread1);
+    }
+
+}
