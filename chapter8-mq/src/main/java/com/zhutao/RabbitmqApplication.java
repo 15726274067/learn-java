9b74173cc4a8e12306067c567798e2a0a37a63aa
zhutao <693117552@qq.com>
2019-02-26 19:54:59 +0800

    bug fix

--- chapter8-mq/src/main/java/com/zhutao/RabbitmqApplication.java
+++ chapter8-mq/src/main/java/com/zhutao/RabbitmqApplication.java
@@ -1,11 +1,10 @@
-package com.zhutao.rabbitmq;
+package com.zhutao;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 
 
 /**
