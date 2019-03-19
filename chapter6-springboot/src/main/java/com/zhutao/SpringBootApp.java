bddcc0f06784faa8493e69b633d746063a663436
zhutao <693117552@qq.com>
2019-03-19 15:31:25 +0800

    springboot集成swagger2

---
+++ chapter6-springboot/src/main/java/com/zhutao/SpringBootApp.java
@@ -0,0 +1,16 @@
+package com.zhutao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019/3/19 15:22
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class SpringBootApp {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootApp.class);
+    }
+}
---
+++ chapter6-springboot/src/main/java/com/zhutao/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.zhutao.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * Swagger2 注解简介
+ * @Author: zhutao
+ * @Date: 2019/3/19 15:04
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/user")
+@Api
+public class UserController {
+
+    @GetMapping("")
+    @ApiOperation(value = "用户列表", notes = "获取用户列表接口")
+    public Map<String, Object> userList(){
+        return null;
+    }
+
+    @PostMapping("")
+    @ApiOperation(value = "创建用户", notes = "创建用户 post")
+    @ApiParam(name = "name", value = "string", defaultValue = "hhh", required = true)
+    public Map<String, Object> saveUser(@RequestBody String name){
+        return null;
+    }
+}
