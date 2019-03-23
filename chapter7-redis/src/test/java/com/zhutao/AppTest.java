e1809d9285c6d28901c8ce5beedacc70e5b724ff
zhutao <693117552@qq.com>
2019-03-23 21:49:12 +0800

    简单的lru cache linkedHashMap实现

--- chapter7-redis/src/test/java/com/zhutao/AppTest.java
+++ chapter7-redis/src/test/java/com/zhutao/AppTest.java
@@ -2,6 +2,7 @@ package com.zhutao;
 
 import static org.junit.Assert.assertTrue;
 
+import com.zhutao.lru.LRUCache;
 import org.junit.Test;
 
 /**
@@ -16,5 +17,25 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testLRU(){
+        LRUCache<String, Object> lruCache = new LRUCache<>();
+        lruCache.put("key1", "value1");
+        lruCache.put("key2", "value2");
+        lruCache.get("key1");
+        lruCache.put("key3", "value3");
+        lruCache.put("key4", "value4");
+        lruCache.put("key5", "value5");
+        /**
+         * key: key2 value: value2
+         * key: key3 value: value3
+         * key: key4 value: value4
+         * key: key5 value: value5
+         */
+        lruCache.forEach(( k, v ) -> {
+            System.out.println("key: " + k + " value: " + v);
+        });
     }
 }
