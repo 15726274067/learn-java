2d25c82e116af21c6915094755a0d56d8f8b253c
zhutao <693117552@qq.com>
2019-04-17 20:48:34 +0800

    java-core: reflect

--- chapter1-java-core/src/main/java/com/zhutao/javacore/basic/reflect/Classinfo.java
+++ chapter1-java-core/src/main/java/com/zhutao/javacore/basic/reflect/Classinfo.java
@@ -1,5 +1,7 @@
 package com.zhutao.javacore.basic.reflect;
 
+import java.util.ArrayList;
+
 /**
  * 1. 运行时获取类型信息和资源
  * 2. 类加载机制
@@ -9,6 +11,18 @@ package com.zhutao.javacore.basic.reflect;
  */
 public class Classinfo {
     public static void main(String[] args){
+        ArrayList<String> arrayList = new ArrayList<>();
+        System.out.println(Classinfo.class);
+        Class<?> cls = arrayList.getClass();
+        System.out.println(cls.getTypeName());
 
+        /**
+         * 加载类
+         */
+        try {
+            Class.forName("");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
