b12aad66eaf8420da66a3ec3752618fae00bebec
zhutao2766 <zhutao@dding.cn>
2019-06-26 20:36:12 +0800

    模板模式

---
+++ chapter1-java-core/src/main/java/com/zhutao/gof/behavior/template/Client.java
@@ -0,0 +1,16 @@
+package com.zhutao.gof.behavior.template;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019-06-26 20:35
+ * @Version 1.0
+ */
+public class Client {
+    public static void main(String[] args) {
+        CaffeineBeverage caffeineBeverage = new Coffee();
+        caffeineBeverage.prepareRecipe();
+        System.out.println("-----------");
+        caffeineBeverage = new Tea();
+        caffeineBeverage.prepareRecipe();
+    }
+}
