dce1ecd652f9078a0534f82290cfff6819044be9
zhutao2766 <zhutao@dding.cn>
2019-06-28 20:14:54 +0800

    观察者模式

---
+++ chapter1-java-core/src/main/java/com/zhutao/gof/behavior/observer/ObserverB.java
@@ -0,0 +1,20 @@
+package com.zhutao.gof.behavior.observer;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019-06-28 20:01
+ * @Version 1.0
+ */
+public class ObserverB implements Observer {
+    public ObserverB(Subject subject) {
+        subject.registerObserver(this);
+    }
+
+    public ObserverB() {
+    }
+
+    @Override
+    public void update(int value) {
+        System.out.println("this is ObserverB, value: " + value);
+    }
+}
---
+++ chapter1-java-core/src/main/java/com/zhutao/gof/behavior/observer/Subject.java
@@ -0,0 +1,14 @@
+package com.zhutao.gof.behavior.observer;
+
+/**
+ * @Author: zhutao
+ * @Date: 2019-06-28 19:44
+ * @Version 1.0
+ */
+public interface Subject {
+    void registerObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void notifyObserver();
+}
