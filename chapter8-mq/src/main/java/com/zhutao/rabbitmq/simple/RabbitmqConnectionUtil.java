3448566791028acc12c1aa1864075136681863bc
zhutao2766 <zhutao@dding.cn>
2019-08-22 14:14:25 +0800

    rabbitmq 自动恢复

--- chapter8-mq/src/main/java/com/zhutao/rabbitmq/simple/RabbitmqConnectionUtil.java
+++ chapter8-mq/src/main/java/com/zhutao/rabbitmq/simple/RabbitmqConnectionUtil.java
@@ -2,6 +2,7 @@ package com.zhutao.rabbitmq.simple;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.impl.DefaultCredentialsProvider;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -17,6 +18,9 @@ public class RabbitmqConnectionUtil {
         connectionFactory.setHost("127.0.0.1");
         connectionFactory.setPort(5672);
 
+        // 一定要这个才能自动恢复
+        connectionFactory.setCredentialsProvider(new DefaultCredentialsProvider("", ""));
+        connectionFactory.setAutomaticRecoveryEnabled(true);
         return connectionFactory.newConnection();
     }
 }
